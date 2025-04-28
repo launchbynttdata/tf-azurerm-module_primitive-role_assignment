5208c6ccecb0efc3aaa101b80a641a1b2963908e
Kevin Allen <160540382+kevin11-allen-nttd@users.noreply.github.com>
2025-04-28 16:02:04 -0400

    change default principal type (#8)
    * change default principal type
    
    * empty commit
    
    * update variable description

--- variables.tf
+++ variables.tf
@@ -40,8 +40,8 @@ variable "principal_id" {
 variable "principal_type" {
   description = <<EOT
     (Optional) The type of Principal to assign the Role Definition to. Changing this forces a new resource to be created.
-    Possible values are User, Group, ServicePrincipal, and Application. Default is User.
+    Possible values are User, Group, ServicePrincipal, and Application. Default is ServicePrincipal.
   EOT
   type        = string
-  default     = "User"
+  default     = "ServicePrincipal"
 }
